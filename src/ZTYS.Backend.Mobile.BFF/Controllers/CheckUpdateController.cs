473623e3ac0b858a0cfec42541a5e4d30ac5785d
Cheng Tao <cttoy@hotmail.com>
2020-10-06 22:20:06 +0800

    增加 AppVersion 类及相应的控制器

---
+++ src/ZTYS.Backend.Mobile.BFF/Controllers/CheckUpdateController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ZTYS.Backend.Mobile.BFF.Models;
+using Microsoft.Extensions.Logging;
+
+namespace ZTYS.Backend.Mobile.BFF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CheckUpdateController : ControllerBase
+    {
+        private readonly ILogger<CheckUpdateController> _logger;
+
+        public CheckUpdateController(ILogger<CheckUpdateController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serviceType"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public AppVersion Get(int versionCode, string versionName, string platformType)
+        {
+            var newAppVersion = new AppVersion
+            {
+                Id = "1",
+                VersionCode = 130,
+                VersionName = "1.6.1",
+                UpdateDigest = "测试",
+                ForcedUpdate = true,
+                PackageDownloadUrl = "http://www.zhangtongyishui.com"
+            };
+
+            return newAppVersion;
+        }
+    }
+}
---
+++ src/ZTYS.Backend.Mobile.BFF/Models/AppVersion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZTYS.Backend.Mobile.BFF.Models
+{
+    public class AppVersion
+    {
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 采用持续增加的整数，如以日期+三位数字的方式
+        /// 如：20201006001
+        /// </summary>
+        public int VersionCode { get; set; }
+        public string VersionName { get; set; }
+
+        /// <summary>
+        /// iOS,Android等
+        /// </summary>
+        public string PlatformType { get; set; }
+        public string UpdateDigest { get; set; }
+
+        /// <summary>
+        /// 是否强制升级
+        /// </summary>
+        public bool ForcedUpdate { get; set; }
+
+        /// <summary>
+        /// 更新包下载地址
+        /// </summary>
+        public string PackageDownloadUrl { get; set; }
+        public DateTime DatePublished { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+        /// <summary>
+        /// 数据行状态
+        /// </summary>
+        public bool RowStatus { get; set; }
+        /// <summary>
+        /// 行版本的跟踪属性
+        /// https://docs.microsoft.com/zh-cn/aspnet/core/data/ef-rp/concurrency?view=aspnetcore-3.1&tabs=visual-studio#add-a-tracking-property
+        /// </summary>
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
