b8267fa8e6702b44d3e5ab1eb0f5f60bdea991ab
Cheng Tao <cttoy@hotmail.com>
2020-10-14 00:58:38 +0800

    修改版本号为1.0.6

--- src/ZTYS.Backend.Mobile.BFF/Controllers/CheckUpdateController.cs
+++ src/ZTYS.Backend.Mobile.BFF/Controllers/CheckUpdateController.cs
@@ -30,7 +30,7 @@ namespace ZTYS.Backend.Mobile.BFF.Controllers
         {
             //JSON中的换行符 \n 需要转义为 \\n
             //换行符后面要紧接内容
-            var newVersionCode = 105;
+            var newVersionCode = 106;
 
             if (versionCode < newVersionCode)
             {
@@ -38,10 +38,10 @@ namespace ZTYS.Backend.Mobile.BFF.Controllers
                 {
                     Id = "1",
                     VersionCode = newVersionCode,
-                    VersionName = "1.0.5",
-                    UpdateDigest = "完善“我的”页面，增加“检查更新”和“关于”",
+                    VersionName = "1.0.6",
+                    UpdateDigest = "1.修改路况地图的默认中心为县府 \\n2.修改加油站和车辆维修地图的中心为第一个商家的坐标",
                     ForcedUpdate = false,
-                    PackageDownloadUrl = "https://ysxjtysj.oss-cn-beijing.aliyuncs.com/ztys/ztys-1.0.5.apk"
+                    PackageDownloadUrl = "https://ysxjtysj.oss-cn-beijing.aliyuncs.com/ztys/ztys-1.0.6.apk"
                 };
 
                 return newAppVersion;
